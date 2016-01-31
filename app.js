397cd1cbc54f4d89536a71a52e2c48a6a5319652
jtabach <jeff.n.tabachnick@gmail.com>
2016-01-31 15:50:50 -0800

    connect to mongodb

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ var users = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/mongoexpresstest', function(err) {
+  if (err) return console.log('Error connecting to mongo database:', err);
+  console.log('connected');
+})
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -22,8 +29,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 app.use('/users', users);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
